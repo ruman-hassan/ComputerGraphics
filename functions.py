0c54d1e1cdec171fcc104634e44be02524bc6249
Kuria Githinji <98692698+kuriofoolio@users.noreply.github.com>
2023-10-01 23:01:39 +0300

    added main.py and functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+# make necessary imports
+import os
+import pandas as pd
+
+
+def convert_jsonl_to_xlsx():
+    # Set the paths
+    jsonl_folder = '1.0/data'
+    output_folder = 'output_xlsx'
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Loop through JSONL files and convert each to an Excel file
+    for filename in os.listdir(jsonl_folder):
+        if filename.endswith('.jsonl'):
+            # Construct file paths
+            jsonl_file_path = os.path.join(jsonl_folder, filename)
+            xlsx_file_path = os.path.join(output_folder, os.path.splitext(filename)[0] + '.xlsx')
+
+            # Read JSONL into a DataFrame
+            data = pd.read_json(jsonl_file_path, lines=True)
+
+            # Write the DataFrame to an Excel file
+            data.to_excel(xlsx_file_path, index=False)
