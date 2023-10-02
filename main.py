3ecdd895ba0b50c2b28f40677b044227d7e123d1
kuria  githinji <kurialancelot@gmail.com>
2023-10-02 14:27:07 +0300

    added path to /data and /output_xlsx as global vars

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from functions import convert_jsonl_to_xlsx
 
 if __name__ == '__main__':
-    # app.run(main)
+    # Set the path to /data where the jsonl files are located
+    jsonl_folder = 'Your path to /data'
+
+    # Set the path to /output_xlsx where the xlsx files will be located
+    output_folder = 'specify location for /output_xlsx'
 
     # call the function for converting each jsonl files to xlsx files
-    convert_jsonl_to_xlsx()
+    convert_jsonl_to_xlsx(jsonl_folder, output_folder)
